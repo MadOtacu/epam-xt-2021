1b8f5e292f147c438228169d0cfa4ae732d84ff5
MadOtacu <pipboy1917@gmail.com>
2021-02-20 17:08:35 +0400

    Idk why but indexes are different. I commit it anyway.

--- Task 1/Task 1.1/1.1.1 RECTANGLE/1.1.1 RECTANGLE/Program.cs
+++ Task 1/Task 1.1/1.1.1 RECTANGLE/1.1.1 RECTANGLE/Program.cs
@@ -6,7 +6,20 @@ namespace _1._1._1_RECTANGLE
     {
         static void Main(string[] args)
         {
-
+            int a = 0;
+            int b = 0;
+            while (a <= 0 || b <= 0)
+            {
+                Console.Write("Введите значение a:");
+                a = Int32.Parse(Console.ReadLine());
+                Console.Write("Введите значение b:");
+                b = Int32.Parse(Console.ReadLine());
+                if (a <= 0 || b <= 0)
+                {
+                    Console.WriteLine("Введите значения больше нуля!");
+                }
+            }
+            Console.WriteLine($"Площадь прямоугольника равна {a * b}");
         }
     }
 }
