0cb34fe6644f907e93a075b068a851f848195f7c
MadOtacu <pipboy1917@gmail.com>
2021-02-21 19:34:06 +0400

    Task 1.1.2 added

---
+++ Task 1/Task 1.1/1.1.2. TRIANGLE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._1._2._TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите длину строки:");
+            a = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
