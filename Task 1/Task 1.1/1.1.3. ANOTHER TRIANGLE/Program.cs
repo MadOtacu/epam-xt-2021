eb8b2da06bdcca8b3e3e1b86190de30fe57f5b9f
MadOtacu <pipboy1917@gmail.com>
2021-02-21 21:45:46 +0400

    Commit after closing the project

---
+++ Task 1/Task 1.1/1.1.3. ANOTHER TRIANGLE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._1._3._ANOTHER_TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
