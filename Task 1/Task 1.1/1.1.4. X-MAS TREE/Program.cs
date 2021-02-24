f16933fe25bc64b486ca952d981a6ab3457ca1bf
MadOtacu <pipboy1917@gmail.com>
2021-02-24 17:50:04 +0400

    Add task 1.1.4

---
+++ Task 1/Task 1.1/1.1.4. X-MAS TREE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._1._4._X_MAS_TREE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк:");
+            if (Int32.TryParse(Console.ReadLine(), out int height))
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int o = 0; o <= i; o++)
+                    {
+                        for (int j = o; j < height - 1; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int k = 0; k < o; k++)
+                        {
+                            Console.Write("**");
+                        }
+                        Console.WriteLine("*");
+                    }
+                }
+            }
+            else { Console.WriteLine("Введите число!"); }
+        }
+    }
+}
