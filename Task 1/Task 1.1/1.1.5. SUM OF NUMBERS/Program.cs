4cfee6785c4616929bda1b3836a6492b1ea3e296
MadOtacu <pipboy1917@gmail.com>
2021-03-01 18:08:45 +0400

    Add task 1.1.5

---
+++ Task 1/Task 1.1/1.1.5. SUM OF NUMBERS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._1._5._SUM_OF_NUMBERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            int sum = 0;
+            while (length < 1000)
+            {
+                if (length % 3 == 0 || length % 5 == 0)
+                {
+                    sum += length;
+                }
+                length++;
+            }
+            Console.WriteLine($"Сумма всех чисел кратных 3 и 5 равна {sum}");
+        }
+    }
+}
