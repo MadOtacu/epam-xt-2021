2dfd3088f843a9b3d3d203098b46f5d24f4b51e7
MadOtacu <pipboy1917@gmail.com>
2021-03-03 17:33:17 +0400

    add task 1.1.6

---
+++ Task 1/Task 1.1/1.1.6. FONT ADJUSTMENT/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._1._6._FONT_ADJUSTMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32.TryParse(Console.ReadLine(), out int temp);
+            List<string> MyList = new List<string>();
+            do
+            {
+                switch (temp)
+                {
+                    case 1:
+                        if (MyList.Contains("Bold")) MyList.Remove("Bold");
+                        else
+                        {
+                            MyList.Add("Bold");
+                        }
+                        Console.WriteLine(string.Join(", ", MyList));
+                        break;
+                    case 2:
+                        if (MyList.Contains("Italic")) MyList.Remove("Italic");
+                        else
+                        {
+                            MyList.Add("Italic");
+                        }
+                        Console.WriteLine(string.Join(", ", MyList));
+                        break;
+                    case 3:
+                        if (MyList.Contains("Underline")) MyList.Remove("Underline");
+                        else
+                        {
+                            MyList.Add("Underline");
+                        }
+                        Console.WriteLine(string.Join(", ", MyList));
+                        break;
+                    default:
+                        Console.WriteLine("Введите значение от 1 до 3!");
+                        break;
+                }
+            }
+            while (int.TryParse(Console.ReadLine(), out temp));
+        }
+    }
+}
