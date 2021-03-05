8023cca085f1ffb513e49de6498f27b94c314d7f
MadOtacu <pipboy1917@gmail.com>
2021-03-05 22:09:55 +0400

    add task 1.1.8

---
+++ Task 1/Task 1.1/1.1.8. NO POSITIVE/1.1.8. NO POSITIVE/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1._1._8._NO_POSITIVE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] arr = new int[10, 10, 5];
+            Random rand = new Random();
+            for (int o = 0; o < 10; o++)
+            {
+                for (int p = 0; p < 10; p++)
+                {
+                    for (int q = 0; q < 5; q++)
+                    {
+                        arr[o, p, q] = rand.Next(500);
+                    }
+                }
+            }
+            for (int o = 0; o < 10; o++)
+            {
+                for (int p = 0; p < 10; p++)
+                {
+                    for (int q = 0; q < 5; q++)
+                    {
+                        if (arr[o, p, q] > 0)
+                        {
+                            arr[o, p, q] = 0;
+                            Console.WriteLine(arr[o, p, q]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
