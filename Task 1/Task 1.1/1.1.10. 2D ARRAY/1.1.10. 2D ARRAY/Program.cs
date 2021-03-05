6560e7a14e0997376d0b379ce404badf3da34002
MadOtacu <pipboy1917@gmail.com>
2021-03-06 00:39:09 +0400

    add task 1.1.9 and 1.1.10

---
+++ Task 1/Task 1.1/1.1.10. 2D ARRAY/1.1.10. 2D ARRAY/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._1._10._2D_ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10, 10];
+            Random rand = new Random();
+            int sum = 0;
+            for (int k = 0; k < 5; k++)
+            {
+                for (int l = 0; l < 10; l++)
+                {
+                    arr[k, l] = rand.Next(100);
+                }
+            }
+            for (int o = 0; o < 10; o++)
+            {
+                for (int p = 0; p < 10; p++)
+                {
+                    if ((o + p + 2) % 2 == 0)
+                    {
+                        sum += arr[o,p];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Task 1/Task 1.1/1.1.9. NON-NEGATIVE SUM/1.1.9. NON-NEGATIVE SUM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._1._9._NON_NEGATIVE_SUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {1, -2, 3, -4, 5, -6, 7, -8, 9, -10, 11, -12, 13, -14, 15};
+            int sum = 0;
+            for (int o = 0; o < arr.Length; o++)
+            {
+                if (arr[o] < 0)
+                {
+                    sum += arr[o];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
