38e1e833a00c381fb41f069fe19f6dd4468a09d7
MadOtacu <pipboy1917@gmail.com>
2021-06-06 22:44:28 +0400

    add task 7.1

---
+++ Task 7/Task 7.1/script.js
@@ -0,0 +1,5 @@
+function DoubleDestroyer(){
+    var inputString = "у попа?была   собака"
+    inputString.split(' ').join(',').split('?').join(',').split('!').join(',').split(':').join(',').split(';').join(',').split('.').join(',').split(',')
+    console.log(inputString)
+}
