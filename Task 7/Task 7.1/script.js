3aa19fae9bf1bb4b84146c0ca8ca4c8e2a3ad8b5
MadOtacu <pipboy1917@gmail.com>
2021-06-07 22:44:01 +0400

    update task 7.1

--- Task 7/Task 7.1/script.js
+++ Task 7/Task 7.1/script.js
@@ -1,5 +1,26 @@
 function DoubleDestroyer(){
-    var inputString = "у попа?была   собака"
-    inputString.split(' ').join(',').split('?').join(',').split('!').join(',').split(':').join(',').split(';').join(',').split('.').join(',').split(',')
-    console.log(inputString)
+    let inputString = "у попа?была   собака";
+    let tempArray;
+    let arrayToDelete;
+    let inputArray = inputString.split(' ').join(',').split('?').join(',').split('!').join(',').split(':').join(',').split(';').join(',').split('.').join(',').split(',');
+    console.log(inputArray);
+    for (let i = 0; i <= inputArray.length; i++){
+        if (inputArray[i] == ""){
+            inputArray.splice(i, 1);
+            i--;
+        }
+    }
+    console.log(inputArray);
+    for (let m = 0; m < inputArray.length; m++) {
+        tempArray = inputArray[m].split("");
+        console.log(tempArray);
+        for (let j = 0; j < tempArray.length; j++) {
+            for (let k = j + 1; k <= tempArray.length; k++) {
+                if (tempArray[j] == tempArray[k]) {
+                    arrayToDelete.push(tempArray[j]);
+                }
+            }
+        }
+    }
+    console.log(arrayToDelete);
 }
